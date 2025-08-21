b936fe286c48884cfe37ec9c66d7c544a77f7c7f
Tito <titoallakhairi@mail.ugm.ac.id>
2025-08-21 10:32:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/introlagi/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.introlagi
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.introlagi.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var number = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        with(binding){
+            txtNumber.text = number.toString()
+            btnCount.setOnClickListener {
+                number++
+                txtNumber.text = number.toString()
+            }
+            btnToast.setOnClickListener {
+                Toast.makeText(this@MainActivity, "Count $number",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
